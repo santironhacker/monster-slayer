d97ede76d08ffeba46ee2e088b15a4457fa676b3
Santiago Ramos <santi92.network@gmail.com>
2024-03-13 16:40:17 +0700

    [main] Created attackMonster and attackMonster basic methods and getRamdomValue helper function.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const app = Vue.create({});
+function getRamdomValue(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.create({
+	data() {
+		return {
+			playerHealth: 100,
+			monsterHealth: 100,
+		}
+	},
+	methods: {
+		attackMonster() {
+			const attackValue = getRamdomValue(5, 12);
+			this.monsterHealth -= attackValue;
+			this.attackPlayer();
+		},
+		attackPlayer() {
+			const attackValue = getRamdomValue(8, 15);
+			this.playerHealth -= attackValue
+		}
+	}
+});
 
 app.mount('#game');
