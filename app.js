6acaec1e7dc07c4d21b5d4666b5a5fcd023fa9df
Santiago Ramos <santi92.network@gmail.com>
2024-03-13 17:56:52 +0700

    [main] Add Game Over section and watchers to compute end of game state.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
 			playerHealth: 100,
 			monsterHealth: 100,
 			currentRound: 0,
+			winner: null,
 		}
 	},
 	computed: {
@@ -23,6 +24,22 @@ const app = Vue.createApp({
 		mayPlayerHeal() {
 			return this.playerHealth >= 100;
 		}
+	},
+	watch: {
+		playerHealth(value) {
+			if(value <= 0 && this.monsterHealth <= 0) {
+				this.winner = 'draw';
+			} else if (value <= 0) {
+				this.winner = 'monster';
+			}
+		},
+		monsterHealth(value) {
+			if(value <= 0 && this.playerHealth <= 0) {
+				this.winner = 'draw';
+			} else if (value <= 0) {
+				this.winner = 'player';
+			}
+		},
 	},
 	methods: {
 		attackMonster() {
