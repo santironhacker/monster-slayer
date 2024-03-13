0297fe12fcc24b25a0a68ea51a6b52397e069b49
Santiago Ramos <santi92.network@gmail.com>
2024-03-13 17:00:18 +0700

    [main] Linked button to attackMonster and link computed properties to modify health bar width.

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ function getRamdomValue(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
-const app = Vue.create({
+const app = Vue.createApp({
 	data() {
 		return {
 			playerHealth: 100,
 			monsterHealth: 100,
+		}
+	},
+	computed: {
+		monsterBarStyles() {
+			return { width: this.monsterHealth + '%' }
+		},
+		playerBarStyles() {
+			return { width: this.playerHealth + '%' }
 		}
 	},
 	methods: {
@@ -19,7 +27,7 @@ const app = Vue.create({
 			const attackValue = getRamdomValue(8, 15);
 			this.playerHealth -= attackValue
 		}
-	}
+	},
 });
 
 app.mount('#game');
