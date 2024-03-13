9c04f1e9434fc1cc5354475800408d704bf7e7b5
Santiago Ramos <santi92.network@gmail.com>
2024-03-13 17:13:50 +0700

    [main] Added a specialAttackMonster method and checked its usage for every 3 turns. Added turns counter for each player action.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
 		return {
 			playerHealth: 100,
 			monsterHealth: 100,
+			currentRound: 0,
 		}
 	},
 	computed: {
@@ -15,18 +16,28 @@ const app = Vue.createApp({
 		},
 		playerBarStyles() {
 			return { width: this.playerHealth + '%' }
+		},
+		mayUseSpecialAttack() {
+			return this.currentRound % 3 !== 0;
 		}
 	},
 	methods: {
 		attackMonster() {
+			this.currentRound++;
 			const attackValue = getRamdomValue(5, 12);
+			this.monsterHealth -= attackValue;
+			this.attackPlayer();
+		},
+		specialAttackMonster() {
+			this.currentRound++;
+			const attackValue = getRamdomValue(10, 25);
 			this.monsterHealth -= attackValue;
 			this.attackPlayer();
 		},
 		attackPlayer() {
 			const attackValue = getRamdomValue(8, 15);
 			this.playerHealth -= attackValue
-		}
+		},
 	},
 });
 
